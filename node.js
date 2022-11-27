309e9a058b9ce3b6341e13c1b464d7cc5c8a6ef6
karlarele <110740286+karlarele@users.noreply.github.com>
2022-11-27 19:12:40 -0300

    Create node.js

---
+++ node.js
@@ -0,0 +1,22 @@
+	Welcome to Node.js v18.12.1.
+Type ".help" for more information.
+> var deuses = ['Aegir','Audi','Balder']
+undefined
+> deuses.push('Loki')
+4
+> deuses.push('Odin')
+5
+> deuses.push('Frey')
+6
+> console.log('deuses')var deuses = ['Aegir','Audi','Balder']
+console.log('deuses')var deuses = ['Aegir','Audi','Balder']
+                     ^^^
+
+Uncaught SyntaxError: Unexpected token 'var'
+> deuses.push('Loki')
+7
+> deuses.push('Odin')
+8
+> deuses.push('Frey')
+9
+> console.log('deuses')
